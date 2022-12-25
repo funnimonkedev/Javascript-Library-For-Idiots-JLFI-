93184ca1c6017f0e1803eb0dbe3f0298dbd77920
funnimonkedev <113639481+funnimonkedev@users.noreply.github.com>
2022-12-25 13:51:56 -0500

    Update library.js
    (3rd?)

--- library.js
+++ library.js
@@ -2,36 +2,40 @@
 
 
 
+
 function $(selector) {
 const self = {
-element: document.querySelector(selector),
+element: document.querySelector(selector), //selects the element defined
 html: () => self.element,
 on: (event, callback) => {
-self.element.addEventListener(event,callback)
+self.element.addEventListener(event,callback) // adds an event listener the 'event' is where you would put 'click' or 'mousedown' and the callback is where you would put 'function() {}' or '()=>'
 },
 hide:() => {
-self.element.style.visibility = 'hidden';
+self.element.style.visibility = 'hidden'; //self explanatory
 },
 show:() => {
-self.element.style.visibility = 'visible';
+self.element.style.visibility = 'visible'; //self explanatory
 },
 attr:(name,value)=> {
-if(value == null) 
-self.element.getAttribute(name);
+if(value == null) //checking if it exists (it is 'the attribute')
+self.element.getAttribute(name); //checking if it exists (it is 'the attribute')
 else
-self.element.setAttribute(name, value);
+self.element.setAttribute(name, value); //adds an attribute if the attribute does not already exist
 },
 color:(color)=> {
-self.element.style.color = color;
+self.element.style.color = color; //self explanatory
 },
 txtSize:(size)=> {
-self.element.style.fontSize = size;
+self.element.style.fontSize = size; //self explanatory
 },
 txt:(text)=> {
-self.element.innerHTML = text;
+self.element.innerHTML = text; //self explanatory
 },
 create:(element, text)=> {
-var para = document.createElement(element); para.innerText = text; document.body.appendChild(para);
+var para = document.createElement(element); para.innerText = text; document.body.appendChild(para); // the 'var para' is supposed to be the method 'document.createElement'
+},
+replacePage:(value)=> {
+document.write(value);
 },
 }
 
@@ -41,9 +45,13 @@ return self;
 
 //examples
 let sIze = 30;
-$('p').create('button', 'hello'); //the selector $(selector) does not matter in this function just put something they're
+$().create('button', 'hello'); //the selector $(selector) does not matter in this function just put something their or (preferably) put nothing
 $('p').on('click',function() {
 $('p').hide();
+});
+
+$('h5').on('click',function() {
+$().replacePage('<h1> hello </h1>'); //the selector $(selector) does not matter in this function just put something their or (preferably) put nothing
 });
 
 $('h1').on('click', function() {
